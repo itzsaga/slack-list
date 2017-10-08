bbe99b83dc75fe3dabc89deece18cb8fec4a4fd7
beanmo <bamorin00@gmail.com>
2017-10-08 11:54:30 -0500

    test commit + slack form

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,10 @@ if (process.env.NODE_ENV !== 'production') {
     module.exports.plugins = (module.exports.plugins || []).concat([
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+         new HtmlWebpackPlugin({  // Also generate a test.html
+          filename: 'new.html',
+          template: './src/new.html'
         })
     ])
 }
