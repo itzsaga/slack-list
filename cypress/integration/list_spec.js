ce81230757441786573dd0f412527d87c20ac7a0
Seth <itz.saga@gmail.com>
2017-10-24 23:57:00 -0500

    Add tests for all current list items

--- cypress/integration/list_spec.js
+++ cypress/integration/list_spec.js
@@ -2,7 +2,9 @@ describe('The List Page', () => {
   beforeEach(() => {
     cy.visit('/list.html')
   })
+
   it('successfully loads', () => {})
+
   context('navbar', () => {
     it("the 'Home' link works", () => {
       cy.contains('Home').click({force: true})
@@ -13,12 +15,43 @@ describe('The List Page', () => {
       cy.title().should('not.equal', 'Error')
     })
   })
+
   context('footer', () => {
     it("the 'MIT License' link works", () => {
       cy.contains('MIT License').click()
     })
     it("the 'CC BY-NC-SA 4.0' link works", () => {
       cy.contains('CC BY-NC-SA 4.0').click()
+    })
+  })
+
+  context('the list', () => {
+
+    context('location based', () => {
+      it("the 'TECH404' link works", () => {
+        cy.contains('TECH404').click()
+      })
+      it("the 'Charleston Tech' link works", () => {
+        cy.contains('Charleston Tech').click()
+      })
+      it("the 'memtech' link works", () => {
+        cy.contains('memtech').click()
+      })
+      it("the 'NashDev' link works", () => {
+        cy.contains('NashDev').click()
+      })
+    })
+
+    context('it based', () => {
+      it("the 'APIs You Won't Hate' link works", () => {
+        cy.contains('APIs You Won't Hate').click()
+      })
+    })
+
+    context('programming based', () => {
+      it("the 'Larachat' link works", () => {
+        cy.contains('Larachat').click()
+      })
     })
   })
 })
