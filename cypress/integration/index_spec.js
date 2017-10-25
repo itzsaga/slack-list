cea85871ac25d054bd1290552ac483abc74d42c8
Seth <itz.saga@gmail.com>
2017-10-24 23:36:59 -0500

    Simplify Index tests with a beforeEach

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,21 +1,19 @@
 describe('The Home Page', () => {
-  it('successfully loads', () => {
+  beforeEach(() => {
     cy.visit('/')
   })
+  it('successfully loads', () => {})
   context('navbar', () => {
-    it("the 'about' link works", () => {
-      cy.visit('/')
+    it("the 'About' link works", () => {
       cy.contains('About').click({force: true})
       cy.title().should('not.equal', 'Error')
     })
   })
   context('footer', () => {
     it("the 'MIT License' link works", () => {
-      cy.visit('/')
       cy.contains('MIT License').click()
     })
     it("the 'CC BY-NC-SA 4.0' link works", () => {
-      cy.visit('/')
       cy.contains('CC BY-NC-SA 4.0').click()
     })
   })
