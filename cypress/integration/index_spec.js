604ee964c6bf5a844cb86d286a751148ffd500f3
Seth <itz.saga@gmail.com>
2017-10-24 23:29:08 -0500

    Add more home page tests

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -2,9 +2,21 @@ describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('/')
   })
-  it("clicking 'about' goes to the about page", () => {
-    cy.visit('/')
-    cy.contains('About').click()
-    cy.title().should('not.equal', 'Error')
+  context('navbar', () => {
+    it("the 'about' link works", () => {
+      cy.visit('/')
+      cy.contains('About').click({force: true})
+      cy.title().should('not.equal', 'Error')
+    })
+  })
+  context('footer', () => {
+    it("the 'MIT License' link works", () => {
+      cy.visit('/')
+      cy.contains('MIT License').click()
+    })
+    it("the 'CC BY-NC-SA 4.0' link works", () => {
+      cy.visit('/')
+      cy.contains('CC BY-NC-SA 4.0').click()
+    })
   })
 })
