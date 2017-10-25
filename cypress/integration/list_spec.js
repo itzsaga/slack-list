812553b6ac02645f3529afac4473404054e0d4a8
Seth <itz.saga@gmail.com>
2017-10-25 00:05:21 -0500

    Better worded tests

--- cypress/integration/list_spec.js
+++ cypress/integration/list_spec.js
@@ -6,21 +6,21 @@ describe('The List Page', () => {
   it('successfully loads', () => {})
 
   context('navbar', () => {
-    it("the 'Home' link works", () => {
+    it("'Home' link works", () => {
       cy.contains('Home').click({force: true})
       cy.title().should('not.equal', 'Error')
     })
-    it("the 'About' link works", () => {
+    it("'About' link works", () => {
       cy.contains('About').click({force: true})
       cy.title().should('not.equal', 'Error')
     })
   })
 
   context('footer', () => {
-    it("the 'MIT License' link works", () => {
+    it("'MIT License' link works", () => {
       cy.contains('MIT License').click()
     })
-    it("the 'CC BY-NC-SA 4.0' link works", () => {
+    it("'CC BY-NC-SA 4.0' link works", () => {
       cy.contains('CC BY-NC-SA 4.0').click()
     })
   })
@@ -28,28 +28,28 @@ describe('The List Page', () => {
   context('the list', () => {
 
     context('location based', () => {
-      it("the 'TECH404' link works", () => {
+      it("'TECH404' link works", () => {
         cy.contains('TECH404').click()
       })
-      it("the 'Charleston Tech' link works", () => {
+      it("'Charleston Tech' link works", () => {
         cy.contains('Charleston Tech').click()
       })
-      it("the 'memtech' link works", () => {
+      it("'memtech' link works", () => {
         cy.contains('memtech').click()
       })
-      it("the 'NashDev' link works", () => {
+      it("'NashDev' link works", () => {
         cy.contains('NashDev').click()
       })
     })
 
     context('it based', () => {
-      it("the 'APIs You Won't Hate' link works", () => {
+      it("'APIs You Won't Hate' link works", () => {
         cy.contains("APIs You Won't Hate").click()
       })
     })
 
     context('programming based', () => {
-      it("the 'Larachat' link works", () => {
+      it("'Larachat' link works", () => {
         cy.contains('Larachat').click()
       })
     })
