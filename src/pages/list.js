dff0b75be11c9b48b79ebf29d89bd18e15251401
Seth <sethalexander@pm.me>
2018-10-29 22:34:20 -0500

    Create list component

---
+++ src/pages/list.js
@@ -0,0 +1,349 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const ListPage = ({ location: { pathname } }) => (
+  <Layout location={pathname}>
+    <section className="section">
+      <div className="container">
+        <div className="content">
+          <a name="location">
+            <h2>Networks Based around a Location</h2>
+          </a>
+          <ul>
+            <li>
+              <h3>Denmark</h3>
+            </li>
+            <ul>
+              <li>
+                <a
+                  href="http://slack.devdk.org/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  DevDK
+                </a>{' '}
+                - Dansk digitalt community — Arrangør af One Day Out, stifter af
+                #devdk — For et bedre community i Danmark
+              </li>
+            </ul>
+            <li>
+              <h3>United States</h3>
+            </li>
+            <ul>
+              <li>
+                <h3>Alabama</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="http://magiccitytech.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Magic City Tech
+                  </a>{' '}
+                  - Magic City Tech is a group for Birmingham area developers,
+                  designers, marketers, business people, hobbyists, students,
+                  and other professionals involved in technology.
+                </li>
+              </ul>
+              <li>
+                <h3>California</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="http://slack.softwarecraftsmanship.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Software Craftsmanship
+                  </a>{' '}
+                  - A network for the Los Angeles tech community
+                </li>
+              </ul>
+              <li>
+                <h3>Colorado</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="https://reichrobert.typeform.com/to/V0WnZf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    NewTech Colorado
+                  </a>{' '}
+                  - New Tech is a community of 12,000 geeks who live and work in
+                  Colorado.
+                </li>
+              </ul>
+              <li>
+                <h3>Florida</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="https://suncoast.io/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Suncoast Developers Guild
+                  </a>{' '}
+                  - We are a collective of software engineers, programmers, and
+                  designers in the Tampa Bay Area.
+                </li>
+              </ul>
+              <li>
+                <h3>Georgia</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="http://tech404.io/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    TECH404
+                  </a>{' '}
+                  - Chat channels for Atlanta area developers, designers,
+                  marketers, business people, hobbyists, students, and other
+                  professionals involved in technology.
+                </li>
+              </ul>
+              <li>
+                <h3>Kentucky</h3>
+              </li>
+              <ul>
+                <li>
+                  <a href="https://slackin.louisvilletech.org/" target="blank">
+                    Louisville Tech / Louisville.IO
+                  </a>{' '}
+                  - a community of technology user groups and meetups in
+                  Louisville, Kentucky.
+                </li>
+              </ul>
+              <li>
+                <h3>MidWest</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="https://midwestdevchat.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    MidWestDevChat.com
+                  </a>{' '}
+                  is an awesome group of developers (software, web, etc) from
+                  the midwest who hang out daily using the great Slack messaging
+                  app.
+                </li>
+              </ul>
+              <li>
+                <h3>New York</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="http://www.nyctechslack.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    New York Tech
+                  </a>{' '}
+                  - A network for the New York Tech scene.
+                </li>
+              </ul>
+              <li>
+                <h3>Oregon</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="http://pdx-startups-slack.herokuapp.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Portland Tech
+                  </a>{' '}
+                  - A network for the Portland Tech scene.
+                </li>
+              </ul>
+              <li>
+                <h3>Pennsylvania</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="https://www.sendgrowth.com/psl"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Philadelphia Startup Leaders
+                  </a>{' '}
+                  - Philadelphia startup leaders.
+                </li>
+              </ul>
+              <li>
+                <h3>South Carolina</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="http://charlestontechnology.com/slack/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Charleston Tech
+                  </a>{' '}
+                  - A network for the Charleston Tech scene.
+                </li>
+              </ul>
+              <li>
+                <h3>Tennessee</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="http://slack.memphistechnology.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    memtech
+                  </a>{' '}
+                  - Memphis technical community chat.
+                </li>
+                <li>
+                  <a
+                    href="https://nashdev.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    NashDev
+                  </a>{' '}
+                  - A network for Nashville developers.
+                </li>
+                <li>
+                  <a
+                    href="https://knoxdevs.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    KnoxDev
+                  </a>{' '}
+                  - Knoxvilles Development Gathering{' '}
+                </li>
+              </ul>
+              <li>
+                <h3>Texas</h3>
+              </li>
+              <ul>
+                <li>
+                  <a
+                    href="http://slack.dfwstartupcommunity.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    DFWStartupCommunity
+                  </a>{' '}
+                  - A network for the Dallas-Fort Worth Area.
+                </li>
+                <li>
+                  <a
+                    href="https://slack.open-austin.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Open Austin
+                  </a>{' '}
+                  - for people interested in open government and civic
+                  technology.
+                </li>
+              </ul>
+            </ul>
+          </ul>
+
+          <a name="it">
+            <h2>Networks Based around IT</h2>
+          </a>
+          <ul>
+            <li>
+              <a
+                href="http://android-united.community/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Android United
+              </a>{' '}
+              - A friendly Slack community of Android developers. We unite
+              people from all over the world. We share our experience, knowledge
+              and relevant news about Android and mobile. We help each other.
+            </li>
+            <li>
+              <a
+                href="https://slack.apisyouwonthate.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                APIs You Won't Hate
+              </a>{' '}
+              - Get your API, REST and HTTP questions answered! We're the
+              largest API-centric Slack channel around..
+            </li>
+            <li>
+              <a
+                href="https://karangoel.typeform.com/to/mEl3P9"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                HackerX
+              </a>{' '}
+              - A community of hackers and makers from all over the world to
+              facilitate networking, Q&A's, feedback and more.
+            </li>
+            <li>
+              <a
+                href="https://signup.hangops.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Hangops
+              </a>{' '}
+              - Awesome discussion with the DevOps community.
+            </li>
+            <li>
+              <a
+                href="https://ios-developers.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                iOS Developers
+              </a>{' '}
+              - A Slack community for iOS developers.
+            </li>
+          </ul>
+
+          <a name="programming">
+            <h2>Networks Based around Programming</h2>
+          </a>
+          <ul>
+            <li>
+              <a
+                href="https://larachat.co/slack"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Larachat
+              </a>{' '}
+              - Talk to some of the best Laravel minds on the web today.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+
+export default ListPage
