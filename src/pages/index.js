24b3f75ef712466fe4a9064be6c39ea85dd0ed95
Seth <sethalexander@pm.me>
2018-10-29 21:48:17 -0500

    Start the cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
@@ -10,7 +9,6 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
