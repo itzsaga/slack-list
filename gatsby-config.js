5ffe0d159c64210177d884fd591badac10742c9a
Seth <sethalexander@pm.me>
2018-10-29 21:07:56 -0500

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Default Starter"
+    title: "Slack List"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "slack-list",
+        short_name: "slack-list",
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
