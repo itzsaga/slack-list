1ce3a7ece85b794b904e64cbc8dcc909fa352f5b
Seth <sethalexander@pm.me>
2018-10-31 21:01:41 -0500

    Switch to js file and export statement, more boilerplate

---
+++ src/data/list.js
@@ -0,0 +1,135 @@
+export default {
+  locationBased: {
+    denmark: [
+      {
+        name: 'DevDK',
+        description:
+          'Dansk digitalt community — Arrangør af One Day Out, stifter af #devdk — For et bedre community i Danmark',
+      },
+    ],
+    'united states': [
+      {
+        alabama: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        california: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        colorado: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        florida: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        georgia: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        kentucky: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        midwest: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        'new york': [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        oregon: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        pennsylvania: [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        'south carolina': [
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        tennessee: [
+          {
+            name: '',
+            description: '',
+          },
+          {
+            name: '',
+            description: '',
+          },
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+      {
+        texas: [
+          {
+            name: '',
+            description: '',
+          },
+          {
+            name: '',
+            description: '',
+          },
+        ],
+      },
+    ],
+  },
+  itBased: [{}],
+  productBased: [{}],
+  programmingBased: [{}],
+  hardwareBased: [{}],
+  financeAndMarketingBased: [{}],
+  miscellaneousBased: [{}],
+}
