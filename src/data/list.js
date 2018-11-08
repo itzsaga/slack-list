ee088619c4c58c13f18a068a90ee155030ea73dc
Seth <sethalexander@pm.me>
2018-11-07 21:30:14 -0600

    Formatting on save

--- src/data/list.js
+++ src/data/list.js
@@ -1,161 +1,176 @@
 export default {
-  locationBased: {
-    denmark: [
-      {
-        name: 'DevDK',
-        description: 'Dansk digitalt community — Arrangør af One Day Out, stifter af #devdk — For et bedre community i Danmark',
-        url: 'http://slack.devdk.org/',
-      },
-    ],
-    'united states': [
-      {
-        alabama: [
-          {
-            name: 'Magic City Tech',
-            description: 'Magic City Tech is a group for Birmingham area developers, designers, marketers, business people, hobbyists, students, and other professionals involved in technology.',
-            url: 'http://magiccitytech.org/',
-          },
-        ],
-      },
-      {
-        california: [
-          {
-            name: 'Software Craftsmanship',
-            description: 'A network for the Los Angeles tech community',
-            url: 'http://slack.softwarecraftsmanship.org/',
-          },
-        ],
-      },
-      {
-        colorado: [
-          {
-            name: 'NewTech Colorado',
-            description: 'New Tech is a community of 12,000 geeks who live and work in Colorado.',
-            url: 'https://reichrobert.typeform.com/to/V0WnZf',
-          },
-        ],
-      },
-      {
-        florida: [
-          {
-            name: 'Suncoast Developers Guild',
-            description: 'We are a collective of software engineers, programmers, and designers in the Tampa Bay Area.',
-            url: 'https://suncoast.io/',
-          },
-        ],
-      },
-      {
-        georgia: [
-          {
-            name: 'TECH404',
-            description: 'Chat channels for Atlanta area developers, designers, marketers, business people, hobbyists, students, and other professionals involved in technology.',
-            url: 'http://tech404.io/',
-          },
-        ],
-      },
-      {
-        kentucky: [
-          {
-            name: 'Louisville Tech / Louisville.IO',
-            description: 'a community of technology user groups and meetups in Louisville, Kentucky.',
-            url: 'https://slackin.louisvilletech.org/',
-          },
-        ],
-      },
-      {
-        midwest: [
-          {
-            name: 'MidWestDevChat.com',
-            description: 'An awesome group of developers (software, web, etc) from the midwest who hang out daily using the great Slack messaging app.',
-            url: 'https://midwestdevchat.com/',
-          },
-        ],
-      },
-      {
-        'new york': [
-          {
-            name: 'New York Tech',
-            description: 'A network for the New York Tech scene.',
-            url: 'http://www.nyctechslack.com/',
-          },
-        ],
-      },
-      {
-        oregon: [
-          {
-            name: 'Portland Tech',
-            description: 'A network for the Portland Tech scene.',
-            url: 'http://pdx-startups-slack.herokuapp.com/',
-          },
-        ],
-      },
-      {
-        pennsylvania: [
-          {
-            name: 'Philadelphia Startup Leaders',
-            description: 'Philadelphia Startup Leaders',
-            url: 'https://www.sendgrowth.com/psl',
-          },
-        ],
-      },
-      {
-        'south carolina': [
-          {
-            name: 'Charleston Tech',
-            description: 'A network for the Charleston Tech scene.',
-            url: 'http://charlestontechnology.com/slack/',
-          },
-        ],
-      },
-      {
-        tennessee: [
-          {
-            name: 'memtech',
-            description: 'Memphis technical community chat.',
-            url: 'http://slack.memphistechnology.org/',
-          },
-          {
-            name: 'NashDev',
-            description: 'A network for Nashville developers.',
-            url: 'https://nashdev.com',
-          },
-          {
-            name: 'KnoxDev',
-            description: 'Knoxvilles Development Gathering',
-            url: 'https://knoxdevs.com',
-          },
-        ],
-      },
-      {
-        texas: [
-          {
-            name: 'DFWStartupCommunity',
-            description: 'A network for the Dallas-Fort Worth Area.',
-            url: 'http://slack.dfwstartupcommunity.com/',
-          },
-          {
-            name: 'Open Austin',
-            description: 'For people interested in open government and civic technology.',
-            url: 'https://slack.open-austin.org/',
-          },
-        ],
-      },
-    ],
-  },
+  locationBased: [
+    {
+      denmark: [
+        {
+          name: 'DevDK',
+          description:
+            'Dansk digitalt community — Arrangør af One Day Out, stifter af #devdk — For et bedre community i Danmark',
+          url: 'http://slack.devdk.org/',
+        },
+      ],
+    },
+    {
+      'united states': [
+        {
+          alabama: [
+            {
+              name: 'Magic City Tech',
+              description:
+                'Magic City Tech is a group for Birmingham area developers, designers, marketers, business people, hobbyists, students, and other professionals involved in technology.',
+              url: 'http://magiccitytech.org/',
+            },
+          ],
+        },
+        {
+          california: [
+            {
+              name: 'Software Craftsmanship',
+              description: 'A network for the Los Angeles tech community',
+              url: 'http://slack.softwarecraftsmanship.org/',
+            },
+          ],
+        },
+        {
+          colorado: [
+            {
+              name: 'NewTech Colorado',
+              description:
+                'New Tech is a community of 12,000 geeks who live and work in Colorado.',
+              url: 'https://reichrobert.typeform.com/to/V0WnZf',
+            },
+          ],
+        },
+        {
+          florida: [
+            {
+              name: 'Suncoast Developers Guild',
+              description:
+                'We are a collective of software engineers, programmers, and designers in the Tampa Bay Area.',
+              url: 'https://suncoast.io/',
+            },
+          ],
+        },
+        {
+          georgia: [
+            {
+              name: 'TECH404',
+              description:
+                'Chat channels for Atlanta area developers, designers, marketers, business people, hobbyists, students, and other professionals involved in technology.',
+              url: 'http://tech404.io/',
+            },
+          ],
+        },
+        {
+          kentucky: [
+            {
+              name: 'Louisville Tech / Louisville.IO',
+              description:
+                'a community of technology user groups and meetups in Louisville, Kentucky.',
+              url: 'https://slackin.louisvilletech.org/',
+            },
+          ],
+        },
+        {
+          midwest: [
+            {
+              name: 'MidWestDevChat.com',
+              description:
+                'An awesome group of developers (software, web, etc) from the midwest who hang out daily using the great Slack messaging app.',
+              url: 'https://midwestdevchat.com/',
+            },
+          ],
+        },
+        {
+          'new york': [
+            {
+              name: 'New York Tech',
+              description: 'A network for the New York Tech scene.',
+              url: 'http://www.nyctechslack.com/',
+            },
+          ],
+        },
+        {
+          oregon: [
+            {
+              name: 'Portland Tech',
+              description: 'A network for the Portland Tech scene.',
+              url: 'http://pdx-startups-slack.herokuapp.com/',
+            },
+          ],
+        },
+        {
+          pennsylvania: [
+            {
+              name: 'Philadelphia Startup Leaders',
+              description: 'Philadelphia Startup Leaders',
+              url: 'https://www.sendgrowth.com/psl',
+            },
+          ],
+        },
+        {
+          'south carolina': [
+            {
+              name: 'Charleston Tech',
+              description: 'A network for the Charleston Tech scene.',
+              url: 'http://charlestontechnology.com/slack/',
+            },
+          ],
+        },
+        {
+          tennessee: [
+            {
+              name: 'memtech',
+              description: 'Memphis technical community chat.',
+              url: 'http://slack.memphistechnology.org/',
+            },
+            {
+              name: 'NashDev',
+              description: 'A network for Nashville developers.',
+              url: 'https://nashdev.com',
+            },
+            {
+              name: 'KnoxDev',
+              description: 'Knoxvilles Development Gathering',
+              url: 'https://knoxdevs.com',
+            },
+          ],
+        },
+        {
+          texas: [
+            {
+              name: 'DFWStartupCommunity',
+              description: 'A network for the Dallas-Fort Worth Area.',
+              url: 'http://slack.dfwstartupcommunity.com/',
+            },
+            {
+              name: 'Open Austin',
+              description:
+                'For people interested in open government and civic technology.',
+              url: 'https://slack.open-austin.org/',
+            },
+          ],
+        },
+      ],
+    },
+  ],
   itBased: [
     {
       name: 'Android United',
-      description: 'A friendly Slack community of Android developers. We unite people from all over the world. We share our experience, knowledge and relevant news about Android and mobile. We help each other.',
+      description:
+        'A friendly Slack community of Android developers. We unite people from all over the world. We share our experience, knowledge and relevant news about Android and mobile. We help each other.',
       url: 'http://android-united.community/',
     },
     {
       name: "APIs You Won't Hate",
-      description: "Get your API, REST and HTTP questions answered! We're the larrgest API-centric Slack channel around.",
+      description:
+        "Get your API, REST and HTTP questions answered! We're the larrgest API-centric Slack channel around.",
       url: 'https://slack.apisyouwonthate.com/',
     },
     {
       name: 'HackerX',
-      description: "A community of hackers and makers from all over the world to facilitate networking, Q&A's, feedback and more.",
+      description:
+        "A community of hackers and makers from all over the world to facilitate networking, Q&A's, feedback and more.",
       url: 'https://karangoel.typeform.com/to/mEl3P9',
     },
     {
