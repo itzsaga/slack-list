70fe2712bcb6d11f5d47a4e24a572b28908f25b8
Seth <sethalexander@pm.me>
2018-11-07 20:50:56 -0600

    Finish filling in JSON data

--- src/data/list.js
+++ src/data/list.js
@@ -3,8 +3,7 @@ export default {
     denmark: [
       {
         name: 'DevDK',
-        description:
-          'Dansk digitalt community — Arrangør af One Day Out, stifter af #devdk — For et bedre community i Danmark',
+        description: 'Dansk digitalt community — Arrangør af One Day Out, stifter af #devdk — For et bedre community i Danmark',
         url: 'http://slack.devdk.org/',
       },
     ],
@@ -12,132 +11,132 @@ export default {
       {
         alabama: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'Magic City Tech',
+            description: 'Magic City Tech is a group for Birmingham area developers, designers, marketers, business people, hobbyists, students, and other professionals involved in technology.',
+            url: 'http://magiccitytech.org/',
           },
         ],
       },
       {
         california: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'Software Craftsmanship',
+            description: 'A network for the Los Angeles tech community',
+            url: 'http://slack.softwarecraftsmanship.org/',
           },
         ],
       },
       {
         colorado: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'NewTech Colorado',
+            description: 'New Tech is a community of 12,000 geeks who live and work in Colorado.',
+            url: 'https://reichrobert.typeform.com/to/V0WnZf',
           },
         ],
       },
       {
         florida: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'Suncoast Developers Guild',
+            description: 'We are a collective of software engineers, programmers, and designers in the Tampa Bay Area.',
+            url: 'https://suncoast.io/',
           },
         ],
       },
       {
         georgia: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'TECH404',
+            description: 'Chat channels for Atlanta area developers, designers, marketers, business people, hobbyists, students, and other professionals involved in technology.',
+            url: 'http://tech404.io/',
           },
         ],
       },
       {
         kentucky: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'Louisville Tech / Louisville.IO',
+            description: 'a community of technology user groups and meetups in Louisville, Kentucky.',
+            url: 'https://slackin.louisvilletech.org/',
           },
         ],
       },
       {
         midwest: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'MidWestDevChat.com',
+            description: 'An awesome group of developers (software, web, etc) from the midwest who hang out daily using the great Slack messaging app.',
+            url: 'https://midwestdevchat.com/',
           },
         ],
       },
       {
         'new york': [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'New York Tech',
+            description: 'A network for the New York Tech scene.',
+            url: 'http://www.nyctechslack.com/',
           },
         ],
       },
       {
         oregon: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'Portland Tech',
+            description: 'A network for the Portland Tech scene.',
+            url: 'http://pdx-startups-slack.herokuapp.com/',
           },
         ],
       },
       {
         pennsylvania: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'Philadelphia Startup Leaders',
+            description: 'Philadelphia Startup Leaders',
+            url: 'https://www.sendgrowth.com/psl',
           },
         ],
       },
       {
         'south carolina': [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'Charleston Tech',
+            description: 'A network for the Charleston Tech scene.',
+            url: 'http://charlestontechnology.com/slack/',
           },
         ],
       },
       {
         tennessee: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'memtech',
+            description: 'Memphis technical community chat.',
+            url: 'http://slack.memphistechnology.org/',
           },
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'NashDev',
+            description: 'A network for Nashville developers.',
+            url: 'https://nashdev.com',
           },
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'KnoxDev',
+            description: 'Knoxvilles Development Gathering',
+            url: 'https://knoxdevs.com',
           },
         ],
       },
       {
         texas: [
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'DFWStartupCommunity',
+            description: 'A network for the Dallas-Fort Worth Area.',
+            url: 'http://slack.dfwstartupcommunity.com/',
           },
           {
-            name: '',
-            description: '',
-            url: '',
+            name: 'Open Austin',
+            description: 'For people interested in open government and civic technology.',
+            url: 'https://slack.open-austin.org/',
           },
         ],
       },
@@ -145,37 +144,37 @@ export default {
   },
   itBased: [
     {
-      name: '',
-      description: '',
-      url: '',
+      name: 'Android United',
+      description: 'A friendly Slack community of Android developers. We unite people from all over the world. We share our experience, knowledge and relevant news about Android and mobile. We help each other.',
+      url: 'http://android-united.community/',
     },
     {
-      name: '',
-      description: '',
-      url: '',
+      name: "APIs You Won't Hate",
+      description: "Get your API, REST and HTTP questions answered! We're the larrgest API-centric Slack channel around.",
+      url: 'https://slack.apisyouwonthate.com/',
     },
     {
-      name: '',
-      description: '',
-      url: '',
+      name: 'HackerX',
+      description: "A community of hackers and makers from all over the world to facilitate networking, Q&A's, feedback and more.",
+      url: 'https://karangoel.typeform.com/to/mEl3P9',
     },
     {
-      name: '',
-      description: '',
-      url: '',
+      name: 'Hangops',
+      description: 'Awesome discussion with the DevOps community.',
+      url: 'https://signup.hangops.com/',
     },
     {
-      name: '',
-      description: '',
-      url: '',
+      name: 'iOS Developers',
+      description: 'A Slack community for iOS developers.',
+      url: 'https://ios-developers.io/',
     },
   ],
   productBased: [],
   programmingBased: [
     {
-      name: '',
-      description: '',
-      url: '',
+      name: 'Larachat',
+      description: 'Talk to some of the best Laravel minds on the web today.',
+      url: 'https://larachat.co/slack',
     },
   ],
   hardwareBased: [],
