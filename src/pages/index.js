f08f290612b600f42652e0dfc72bb151f8b4e439
Seth <sethalexander@pm.me>
2018-11-07 22:17:56 -0600

    Move the list to root route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import ListItem from '../components/listItem'
+import LocationBased from '../components/locationBased'
 
-const IndexPage = ({ location: { pathname }}) => (
+import list from '../data/list'
+
+const IndexPage = ({ location: { pathname } }) => (
   <Layout location={pathname}>
     <section className="section">
       <div className="container">
         <div className="content">
-          <h2>This is the home page.</h2>
+          <LocationBased locationBased={list.locationBased} />
+          <div id="it">
+            <h2>Networks Based around IT</h2>
+          </div>
+          <ul>
+            {list.itBased.map(network => (
+              <ListItem item={network} />
+            ))}
+          </ul>
+          <div id="programming">
+            <h2>Networks Based around Programming</h2>
+          </div>
+          <ul>
+            {list.programmingBased.map(network => (
+              <ListItem item={network} />
+            ))}
+          </ul>
         </div>
       </div>
     </section>
