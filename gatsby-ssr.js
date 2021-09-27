090b9bb1d95a7a440f6944c4153420d4f003e5f5
Seth <setha@hey.com>
2021-09-27 08:34:13 -0500

    remove cloudflare analytics injection

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function onRenderBody({ setPostBodyComponents }) {
-  setPostBodyComponents([
-    <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "c4bfa0dcaced44f7a46bb954ea156cc4"}' />
-  ]);
-}
